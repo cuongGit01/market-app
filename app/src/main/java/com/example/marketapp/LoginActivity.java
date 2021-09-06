01203c0c1f841d140005e9931fc8453723a6d710
DuyNguyenDeveloper <duynq.dev@gmail.com>
2021-09-06 10:15:52 +0700

    Login, Register

--- app/src/main/java/com/example/marketapp/LoginActivity.java
+++ app/src/main/java/com/example/marketapp/LoginActivity.java
@@ -5,11 +5,13 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.ProgressBar;
 import android.widget.TextView;
 
 public class LoginActivity extends AppCompatActivity {
 
     TextView tvSignup;
+    ProgressBar progressBar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -17,11 +19,13 @@ public class LoginActivity extends AppCompatActivity {
         setContentView(R.layout.activity_login);
 
         tvSignup = (TextView) findViewById(R.id.tv_signup);
+        progressBar = (ProgressBar) findViewById(R.id.progressbar);
 
         tvSignup.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                startActivity(new Intent(LoginActivity.this, RegistrationActivity.class));
+                Intent i = new Intent(new Intent(LoginActivity.this, RegistrationActivity.class));
+                startActivity(i);
                 finish();
             }
         });
--- app/src/main/java/com/example/marketapp/RegistrationActivity.java
+++ app/src/main/java/com/example/marketapp/RegistrationActivity.java
@@ -5,23 +5,28 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.ProgressBar;
 import android.widget.TextView;
 
 public class RegistrationActivity extends AppCompatActivity {
 
     TextView tvSignin;
+    ProgressBar progressBar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_registration);
 
+        progressBar = (ProgressBar) findViewById(R.id.progressbar);
         tvSignin = (TextView) findViewById(R.id.tv_signin);
 
         tvSignin.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                startActivity(new Intent(RegistrationActivity.this, LoginActivity.class));
+                Intent i = new Intent(RegistrationActivity.this, LoginActivity.class);
+//                i.putExtra("");
+                startActivity(i);
                 finish();
             }
         });
