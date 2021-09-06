8403ffb4b05bfe2020571fec94266b47fc8fe9af
DuyNguyenDeveloper <duynq.dev@gmail.com>
2021-09-06 09:33:23 +0700

    Login, Register

--- app/src/main/java/com/example/marketapp/LoginActivity.java
+++ app/src/main/java/com/example/marketapp/LoginActivity.java
@@ -2,13 +2,28 @@ package com.example.marketapp;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
 
 public class LoginActivity extends AppCompatActivity {
+
+    TextView tvSignup;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+
+        tvSignup = (TextView) findViewById(R.id.tv_signup);
+
+        tvSignup.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startActivity(new Intent(LoginActivity.this, RegistrationActivity.class));
+                finish();
+            }
+        });
     }
 }
